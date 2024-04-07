d29fa4fbc9e6fc605037f92d2d3047b6d88a9075
dokhanhnam1199 <117411703+dokhanhnam1199@users.noreply.github.com>
2024-04-08 00:38:47 +0700

    .
    .

---
+++ lab 2/AIMS/src/Aims.java
@@ -0,0 +1,18 @@
+public class Aims {
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Inception", "Science Fiction", "Christopher Nolan", 148, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Breaking Bad", "Crime", "Vince Gilligan", 29.99f);
+        anOrder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Family Guy", "Animation",19.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        System.out.println("Total cost is: ");
+        System.out.println(anOrder.totalCost());
+
+    }
+}
